8394fe95f30fc51598cf90a60cf71d8214057677
jose maciel <jose.r.maciel@outlook.com>
2025-06-26 20:07:42 -0300

    adicão dos models para o banco de dados

---
+++ models.py
@@ -0,0 +1,62 @@
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
+
+db = create_engine("sqlite:///banco.db")
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String, nullable=False)
+    senha = Column("senha", String)
+    ativo = Column("ativo", Boolean)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, nome:str, email:str, senha:str, ativo:bool=True, admin:bool=False) -> None:
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+        
+class Pedidos(Base):
+    __tablename__ = "pedidos"
+    
+    # STATUS_PEDIDO = (
+    #     ("PENDENTE", "PENDENTE"),
+    #     ("CANCELADO", "CANCELADO"),
+    #     ("FINALIZADO", "FINALIZADO"),
+    # )
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", String)
+    usuario = Column("usuario", ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
+    # itens = 
+    
+    def __init__(self, id:int, usuario:int, status:str="PENDENTE", preco:float=0) -> None:
+        self.id = id
+        self.usuario = usuario
+        self.preco = preco
+        self.status = status
+        
+class ItemPedido(Base):
+    __tablename__ = "itens_pedido"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    sabor = Column("sabor", String)
+    tamanho = Column("tamanho", String)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+    
+    def __init__(self, quantidade:int, sabor:str, tamanho:str, preco_unitario:float, pedido:int) -> None:
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
