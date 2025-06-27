f21d36cc8b6c405920f57b5e6a46611d8af3f578
jose maciel <jose.r.maciel@outlook.com>
2025-06-27 11:37:14 -0300

    rota de criação de usuário feita

---
+++ routes/auth_routes.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+
+from models import Usuario
+from dependencies import pegar_sessao
+
+auth_router = APIRouter(prefix='/auth', tags=['auth'])
+
+@auth_router.get('/')
+async def home():
+    return {"msg":"rota padrão de autenticação"}
+
+@auth_router.post("/criar-conta")
+async def criar_conta(nome:str, email:str, senha:str, session=Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+
+    if usuario: # Já existe um usuário com esse email
+        return {"msg":"já existe um usuário com esse email"}
+
+    else: # Não existe um usuário com esse email
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        session.commit()
+        return {"msg":"usuário cadastrado com sucesso"}
